5f67a4796ab7f569ea031c701c950ed4217d70f2
Brendan Herger <13herger@gmail.com>
2020-03-31 16:34:09 -0700

    First commit, linked list interface

---
+++ interfaces/algorithms/linked_list.py
@@ -0,0 +1,22 @@
+class LinkedList(object):
+
+    def __init__(self):
+        pass
+
+    def add(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def peak(self):
+        pass
+
+    def find(self):
+        pass
+
+    def contains(self):
+        pass
+
+    def __contains__(self):
+        pass
